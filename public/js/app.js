e34c31a94e7f53ff51e351891fad2971d0cca6a7
Shivam Gupta <shivamgupt8@gmail.com>
2019-07-16 14:37:35 +0530

    remove unnecessory code

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,3 @@
-console.log('clint side js file loaded') 
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')

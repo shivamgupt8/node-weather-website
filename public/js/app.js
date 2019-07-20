125d4a4e35bd1f4920bb03dc92912ac9ce1a53c3
Shivam Gupta <shivamgupt8@gmail.com>
2019-07-20 22:23:23 +0530

    added a space

--- public/js/app.js
+++ public/js/app.js
@@ -2,6 +2,7 @@ const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
+
 weatherForm.addEventListener('submit',(e)=>{
     e.preventDefault()
     const location = search.value
